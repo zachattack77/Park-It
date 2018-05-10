23af146a415fa9418e3b4d1f9f602a585005632c
Alex Shornal <shornality@yahoo.com>
2018-05-10 17:11:05 -0400

    lattitude longitutde functions added

--- parkItCordova/www/js/park-it.js
+++ parkItCordova/www/js/park-it.js
@@ -20,6 +20,30 @@ function onDeviceReady() {
     }
 }
 
+function setParkingLocation(){
+    navigator.geolocation.getCurrentPosition(setParkingLocationSuccess,
+        setParkingLocationError, {enableHighAccuracy:true});
+}
+function setParkingLocationSuccess(position){
+    latitude = position.coords.latitude;
+    storage.setItem("parkedLatitude", latitude);
+    //add statements to store the longitute
+    longitude = position.coords.longitude;
+    storage.setItem("parkedLongitude", longitude);
+
+    //display an alert that shows the latitude and longitude
+    navigator.notification.alert("Parking location saved. (Lat: " + latitude + ", Long: " + longitude) + ")";
+    showParkingLocation();
+}
+
+function setParkingLocationError(error){
+    navigator.notification.alert("Error Code: " + error.code + "\nError Message: " + error.message);
+}
+function showParkingLocation(){
+    navigator.notification.alert("You are parked at Lat: " + storage.getItem("parkedLatitude") + ", Long: " + storage.getItem("parkedLongitude"));
+}
+
+
 function initMap(){
     var grc = {lat: 47.313582, lng: -122.1800072};
 
@@ -36,7 +60,8 @@ function initMap(){
 }
 
 $("#park").click(function () {
-    alert("Set parking location");
+    setParkingLocation();
+    //alert("Set parking location");
 });
 
 $("#retrieve").click(function() {
@@ -45,4 +70,12 @@ $("#retrieve").click(function() {
 
 $("#gotIt").click(function () {
     $("#instructions").slideUp();
+});
+
+$("#directions").ready(function () {
+    $("#directions").hide();
+});
+
+$("#instructions").ready(function () {
+    $("#instructions").hide();
 });
